9abdf9a70edc58d26d5fe7599db420cbdb86bd4c
UdayanChoudhury22 <97618721+UdayanChoudhury22@users.noreply.github.com>
2023-06-29 07:57:45 +0530

    Added solution - LeetHub

--- Binary Search - GFG/binary-search.java
+++ Binary Search - GFG/binary-search.java
@@ -50,21 +50,3 @@ class Solution {
     }
 }
 
-// class Solution {
-//     int binarysearch(int arr[], int n, int k) {
-//         // code here
-//         int start=0;
-//         int end=n-1;
-//         while(start<=end)
-//         {
-//             int mid=start+(end-start)/2;
-//             if(arr[mid]==k)
-//                 return mid;
-//             else if(arr[mid]<k)
-//                 start=mid+1;
-//             else
-//                 end=mid-1;
-//         }
-//         return -1;
-//     }
-// }
