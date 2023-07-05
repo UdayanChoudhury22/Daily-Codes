8f7bf36754dc0aad0282e1f25b03c11386a26643
UdayanChoudhury22 <97618721+UdayanChoudhury22@users.noreply.github.com>
2023-07-05 20:28:25 +0530

    Added solution - LeetHub

--- Cyclically rotate an array by one - GFG/cyclically-rotate-an-array-by-one.java
+++ Cyclically rotate an array by one - GFG/cyclically-rotate-an-array-by-one.java
@@ -44,11 +44,11 @@ class Compute {
     
     public void rotate(int arr[], int n)
     {
-        int num = arr[n-1];
+        int temp = arr[n-1];
         for(int i=n-1;i>0;i--)
         {
             arr[i] = arr[i-1];
         }
-        arr[0] = num;
+        arr[0] = temp;
     }
 }
