062052ff4a144300319a952393f560de931f0356
UdayanChoudhury22 <97618721+UdayanChoudhury22@users.noreply.github.com>
2023-07-07 15:12:54 +0530

    Added solution - LeetHub

---
+++ Bitonic Point - GFG/bitonic-point.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().findMaximum(arr, n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public int findMaximumUtil(int arr[], int low, int high) {
+
+        /* Base Case: Only one element is present in arr[low..high]*/
+        if (low == high) return arr[low];
+
+        /* If there are two elements and first is greater then
+            the first element is maximum */
+        if ((high == low + 1) && arr[low] >= arr[high]) return arr[low];
+
+        /* If there are two elements and second is greater then
+            the second element is maximum */
+        if ((high == low + 1) && arr[low] < arr[high]) return arr[high];
+
+        int mid = (low + high) / 2; /*low + (high - low)/2;*/
+
+        /* If we reach a point where arr[mid] is greater than both of
+            its adjacent elements arr[mid-1] and arr[mid+1], then arr[mid]
+            is the maximum element*/
+        if (arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1]) return arr[mid];
+
+        /* If arr[mid] is greater than the next
+            element and smaller than the previous
+            element then maximum lies on left side of mid */
+        if (arr[mid] > arr[mid + 1] && arr[mid] < arr[mid - 1])
+            return findMaximumUtil(arr, low, mid - 1);
+
+        // when arr[mid] is greater than arr[mid-1]
+        // and smaller than arr[mid+1]
+        else
+            return findMaximumUtil(arr, mid + 1, high);
+    }
+    public int findMaximum(int arr[], int n) {
+        return findMaximumUtil(arr, 0, n - 1);
+    }
+};
