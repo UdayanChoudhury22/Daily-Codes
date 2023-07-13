747574221757a350cf8327901ef3e30da1184169
UdayanChoudhury22 <97618721+UdayanChoudhury22@users.noreply.github.com>
2023-07-13 22:54:52 +0530

    Added solution - LeetHub

---
+++ Find first repeated character - GFG/find-first-repeated-character.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*; 
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine().trim());
+        while(t-- > 0){
+            String s = read.readLine().trim();
+            Solution ob = new Solution();
+            System.out.println(ob.firstRepChar(s));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution 
+{ 
+    String firstRepChar(String s) 
+    { 
+        // code here
+        HashMap<Character,Integer> map = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<s.length();i++)
+        {
+            if(map.containsKey(s.charAt(i)))
+            {
+                sb.append(s.charAt(i));
+                break;
+            }
+            else
+                map.put(s.charAt(i),1);
+        }
+        
+        if(sb.length() == 0)
+            sb.append("-1");
+        return sb.toString();
+    }
+} 
