8bc7c56a7a3a0b1f82681fc9f927677668610e4c
UdayanChoudhury22 <97618721+UdayanChoudhury22@users.noreply.github.com>
2023-07-14 22:07:43 +0530

    Added solution - LeetHub

--- Implement two stacks in an array - GFG/implement-two-stacks-in-an-array.java
+++ Implement two stacks in an array - GFG/implement-two-stacks-in-an-array.java
@@ -47,47 +47,45 @@ class TwoStack
 
 class twoStacks
 {
-    int[] twoStack = new int[100];
-	int top1 = 0, top2 = 50;
-	
-	void push1(int x) {
-		if (top1 < 0) {
-			top1 = 0;
-		} 
-		if (top1 >= 0 && top1 < 50){
-			twoStack[top1++] = x;
-		}
-		
-	}
-	
-	void push2(int x) {
-		if (top2 < 50) {
-			top2 = 50;
-		} 
-		if (top2 >= 50 && top2 < 100){
-			twoStack[top2++] = x;
-		}
-	}
-	
-	int pop1() {
-		top1--;
-		if (top1 < 0) {
-			return -1;
-		} 
-		if (top1 > 49) {
-			top1 = 49;
-		}
-		return twoStack[top1];
-	}
-	
-	int pop2() {
-		top2--;
-		if (top2 < 50) {
-			return -1;
-		} 
-		if (top2 > 99) {
-			top2 = 99;
-		}
-		return twoStack[top2];
-	}
+    int arr[]; int size;
+    int t1, t2;
+    twoStacks()
+    {
+        size=100;  //given in constraint
+        arr =new int[100];
+        t1=-1;t2=size;
+    }
+    //Function to push an integer into the stack1.
+    void push1(int x)
+    {
+      //check if space exists
+      if(t2-t1 >1)
+          arr[++t1]=x;
+    }
+    //Function to push an integer into the stack2.
+    void push2(int x)
+    {
+       if(t2-t1 >1)
+          arr[--t2]=x;
+    }
+    //Function to remove an element from top of the stack1.
+    int pop1()
+    {
+        if(t1>=0)
+        {
+            int x = arr[t1--];
+            return x;
+        }
+        return -1;
+    }
+    //Function to remove an element from top of the stack2.
+    int pop2()
+    {
+        if(t2<size)
+        {
+            int x = arr[t2++];
+            return x;
+        }
+        return -1;
+    }
 }
