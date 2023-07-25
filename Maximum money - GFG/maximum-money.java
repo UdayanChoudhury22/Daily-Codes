4c3b15ac8e97be32d085f3043ad3dd9c74986009
UdayanChoudhury22 <97618721+UdayanChoudhury22@users.noreply.github.com>
2023-07-26 01:09:22 +0530

    Added solution - LeetHub

---
+++ Maximum money - GFG/maximum-money.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String St[] = read.readLine().split(" ");
+            int N = Integer.parseInt(St[0]);
+            int K = Integer.parseInt(St[1]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.maximizeMoney(N,K));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int maximizeMoney(int N , int K) {
+        // code here
+        if(N==1 || N == 2)
+            return K;
+        if(N==0)
+            return 0;
+        else if(N%2 == 0)
+            return (N/2)*K;
+        else
+            return ((N/2)+1)*K;
+    }
+};
