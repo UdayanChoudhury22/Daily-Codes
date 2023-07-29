06baa00befd3a98238885cddb9da3a08b03204bb
UdayanChoudhury22 <97618721+UdayanChoudhury22@users.noreply.github.com>
2023-07-29 07:53:43 +0530

    Added solution - LeetHub

---
+++ Count number of equal pairs in a string - GFG/count-number-of-equal-pairs-in-a-string.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            
+            String s = sc.next ();
+            System.out.println (new Sol().equalPairs (s));
+        }
+        
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Sol
+{
+    long equalPairs (String s)
+    {
+        // your code here
+        int count = 0;
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<s.length();i++)
+        {
+            if(map.containsKey(s.charAt(i)))
+                map.put(s.charAt(i),map.get(s.charAt(i))+1);
+            else
+                map.put(s.charAt(i),1);
+        }
+        
+        for(Map.Entry<Character,Integer> val : map.entrySet())
+        {
+            if(val.getValue() == 1)
+                count += val.getValue();
+            else
+                count += val.getValue()*val.getValue();
+        }
+        
+        return count;
+    }
+}
