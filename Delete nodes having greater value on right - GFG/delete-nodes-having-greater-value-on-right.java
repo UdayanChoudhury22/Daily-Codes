b2d4fefddeaa4a2901359169cc4ceba86d0e4ef7
UdayanChoudhury22 <97618721+UdayanChoudhury22@users.noreply.github.com>
2023-08-29 23:29:17 +0530

    Added solution - LeetHub

--- Delete nodes having greater value on right - GFG/delete-nodes-having-greater-value-on-right.java
+++ Delete nodes having greater value on right - GFG/delete-nodes-having-greater-value-on-right.java
@@ -80,43 +80,42 @@ class Node {
 */
 class Solution
 {
-    static Node reverse(Node head)
-    {
-        Node curr = head;
-        Node pre = null;
-        Node post = null;
-        while(curr!=null)
-        {
-            post = curr.next;
-            curr.next = pre;
-            pre = curr;
-            curr = post;
-        }
-        
-        return pre;
-    }
     Node compute(Node head)
     {
         // your code here
-        head = reverse(head);
-        int max = head.data;
-        Node temp = head.next;
-        
-        Node head1 = new Node(head.data);
-        Node temp1 = head1;
-        
-        while(temp!=null)
-        {
-            if(temp.data >= max)
+        Node new_head =  rev(head);
+        Node curr = new_head;
+        Node temp=null;
+        Node maxNode = curr;
+        while(curr.next!=null){
+            
+          
+          if(curr.next.data < maxNode.data )
             {
-                temp1.next = new Node(temp.data);
-                temp1 = temp1.next;
-                max = temp.data;
+                temp=curr.next;
+                curr.next = temp.next;
             }
-            temp = temp.next;
+            else
+            {
+                curr=curr.next;
+                maxNode=curr;
+            }
         }
-        head1 = reverse(head1);
-        return head1;
+        
+        return rev(new_head);
+        
+    }
+    Node rev(Node head)
+    {
+        Node prev=null, next=null; Node curr=head;
+        
+        while(curr!=null)
+        {
+            next= curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
     }
 }
-  
