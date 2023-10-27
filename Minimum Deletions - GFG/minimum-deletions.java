51223dd73b6154b0ecf015250fbd439cc0851359
UdayanChoudhury22 <97618721+UdayanChoudhury22@users.noreply.github.com>
2023-10-27 23:04:02 +0530

    Added solution - LeetHub

--- Minimum Deletions - GFG/minimum-deletions.java
+++ Minimum Deletions - GFG/minimum-deletions.java
@@ -21,31 +21,35 @@ class GFG{
 // } Driver Code Ends
 
 
-//User function Template for Java
-
 class Solution{
-    static int minimumNumberOfDeletions(String S) {
-        //your code here
+     static int minimumNumberOfDeletions(String S) {
+         int len = longestPalinSubseq(S);
+         return S.length()-len;
+     }
+    static int longestPalinSubseq(String S)
+    {
+        StringBuilder str = new StringBuilder(S);
+        String s1 = S;
+        String s2 = str.reverse().toString();
         int n = S.length();
-    int[][] dp = new int[n][n];
-
-    // Base cases
-    for (int i = 0; i < n; i++) {
-        dp[i][i] = 0;
-    }
-
-    // Fill the dp table
-    for (int len = 2; len <= n; len++) {
-        for (int i = 0; i < n - len + 1; i++) {
-            int j = i + len - 1;
-            if (S.charAt(i) == S.charAt(j)) {
-                dp[i][j] = dp[i + 1][j - 1];
-            } else {
-                dp[i][j] = Math.min(dp[i + 1][j], dp[i][j - 1]) + 1;
+        int dp[][] = new int[n+1][n+1];
+        
+        
+        for( int i =0 ; i<=n ;i++)
+        {
+            for( int j =0; j<=n;j++)
+            {
+                if(i==0 || j==0)
+                dp[i][j] =0; 
+                
+               else if( s1.charAt(i-1)==s2.charAt(j-1) )
+                    dp[i][j] = 1+dp[i-1][j-1];  
+                    
+                else
+                    dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]);
             }
         }
-    }
-
-    return dp[0][n - 1];
+        
+        return dp[n][n];
     }
 }
